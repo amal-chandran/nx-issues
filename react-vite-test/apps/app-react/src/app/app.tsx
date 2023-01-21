30c1f1863e4ebd7dcc64fa24ebdcecb60e1b48c4
Amal Chandran <amalchandrandev@gmail.com>
2023-01-21 14:21:10 +0530

    chore(stack): created react-vite-test project

---
+++ react-vite-test/apps/app-react/src/app/app.tsx
@@ -0,0 +1,13 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+
+export function App() {
+  return (
+    <div>
+      <div>Env Test</div>
+      {/* <div>API URL:{process.env.NX_API_URL}</div> */}
+      {/* <div>{import.meta.env.NX_API_URL}</div> */}
+    </div>
+  );
+}
+
+export default App;
